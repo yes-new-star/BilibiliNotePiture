fcdbc9ea6a607d2164c66c894791cf8a5cc5cb5e
SNY <1532634190@qq.com>
2023-06-07 20:13:15 +0800

    好像出bug了

--- bilibilreply.mjs
+++ bilibilreply.mjs
@@ -12,7 +12,7 @@ import querystring from "querystring";
 
 function bilibili_reply() {
   const myCookie ="";
-  
+
   let reg = /(?<=bili_jct=)[0-9a-f]{32}/;
 
   const reply_csrf = myCookie.match(reg)[0];
@@ -82,53 +82,67 @@ function bilibili_reply() {
                   res.on("end", () => {
                     reply_data = JSON.parse(reply_data);
 
-                    let add_message = '';
+                    let add_message = "";
 
-                    for (let j = 0; j < reply_data.data.seek_root_reply.content.pictures.length; j++) {
-                      add_message += `图片${j+1}地址：${reply_data.data.seek_root_reply.content.pictures[j].img_src}\n`;
+                    for (
+                      let j = 0;
+                      j <
+                      reply_data.data.seek_root_reply.content.pictures.length;
+                      j++
+                    ) {
+                      add_message += `图片${j + 1}地址：${
+                        reply_data.data.seek_root_reply.content.pictures[j]
+                          .img_src
+                      }\n`;
                     }
-                      console.log(
-                        "Response body:",
-                        reply_data.data.seek_root_reply.content.pictures
-                      );
-                      // 回复请求
-                      const datas = {
-                        at_name_to_mid: {},
-                        csrf: reply_csrf,
-                        message: add_message,
-                        oid: data.data.items[at_nums - i - 1].item.subject_id,
-                        parent: data.data.items[at_nums - i - 1].item.source_id,
-                        plat: 1,
-                        root: data.data.items[at_nums - i - 1].item.target_id,
-                        type: 1,
-                      };
-                      const postData = querystring.stringify(datas);
-                      const add_options = {
-                        method: "POST",
-                        hostname: "api.bilibili.com",
-                        path: `/x/v2/reply/add`,
-                        headers: {
-                          Cookie: myCookie,
-                          "Content-Type": "application/x-www-form-urlencoded",
-                        },
-                        withCredentials: true,
-                      };
-                      const add_req = https.request(add_options, (res) => {
-                        console.log(res.statusCode);
-                        let data = "";
-                        res.on("data", (chunk) => {
-                          data += chunk;
-                        });
-                        res.on("end", () => {
-                          console.log('回复请求结果：',data);
-                        });
+                    console.log(
+                      "Response body:",
+                      reply_data.data.seek_root_reply.content.pictures,reply_csrf
+                    );
+                    // 回复请求
+                    const datas = {
+                      type: 1,
+                      oid: data.data.items[at_nums - i - 1].item.subject_id,
+
+                      message: add_message,
+                      root: data.data.items[at_nums - i - 1].item.target_id,
+                      parent: data.data.items[at_nums - i - 1].item.source_id,
+                      jsonp: 'jsonp',
+                      scene: 'msg',
+                      plat: 1,
+                      from: 'im-reply',
+                      build: 0,                      
+                      mobi_app: 'web',
+                      csrf: reply_csrf,
+                      csrf_token: reply_csrf,
+                    };
+                    const postData = querystring.stringify(datas);
+                    const add_options = {
+                      method: "POST",
+                      hostname: "api.bilibili.com",
+                      path: `/x/v2/reply/add`,
+                      headers: {
+                        Cookie: myCookie,
+                        "Content-Type": "application/json;charset=utf-8",
+                      },
+                      withCredentials: true,
+                    };
+                    const add_req = https.request(add_options, (res) => {
+                      console.log(res.statusCode);
+                      let data = "";
+                      res.on("data", (chunk) => {
+                        data += chunk;
                       });
-                      add_req.on("error", (error) => {
-                        console.error("Request error:", error);
+                      res.on("end", () => {
+                        console.log("回复请求结果：", data);
                       });
-                      add_req.write(postData);
-                      add_req.end();
-                    
+                    });
+                    add_req.on("error", (error) => {
+                      console.error("Request error:", error);
+                    });
+                    add_req.write(postData);
+                    console.log('postdata',postData)
+                    add_req.end();
                   });
                 });
                 reply_req.on("error", (error) => {
